37317badd6032d507eda99cb87b20b785e3b10ad
syedayazhasan <142472426+syedayazhasan@users.noreply.github.com>
2024-04-03 15:57:42 +0530

    Update function.c

--- Day 2/function.c
+++ Day 2/function.c
@@ -1,6 +1,25 @@
-#include<stdio.h>
-int main(int argc, char const *argv[])
+ #include <stdio.h>
+int main()
 {
-    /* code */
+    int i,j,n;
+    int k;
+    printf("Enter the number\n");
+    scanf("%d",&n);
+    k=n*2-1;
+    for (i = 1; i <=k; i++)
+    {
+        for (j=1;j<=k;j++)
+        {
+            if (j==i||(j==k-i+1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
     return 0;
 }
